f886b483e422ae3c02666fd47fb7cdfae3dd525c
borodin <m.borodin@top.team>
2025-09-16 14:25:19 +0300

    use pg

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,39 @@
 import {Sequelize} from 'sequelize-typescript';
+import pg from 'pg';
 import {User} from './User.js';
 import {Character} from './Character.js';
 import {Message} from './Message.js';
 import {SpiceTransaction} from './SpiceTransaction.js';
 
 const isDigitalOceanDB = process.env.DATABASE_URL!.includes('ondigitalocean.com');
+console.log('PG');
+// Настройка CA сертификата
+const ca =
+  isDigitalOceanDB && process.env.DATABASE_CA_CERT
+    ? Buffer.from(process.env.DATABASE_CA_CERT, 'base64').toString('utf-8')
+    : undefined;
 
-const sequelizeConfig: any = {
+interface SequelizeConfig {
+  dialect: 'postgres';
+  dialectModule: typeof pg;
+  models: (typeof User | typeof Character | typeof Message | typeof SpiceTransaction)[];
+  logging: boolean;
+  define: {
+    underscored: boolean;
+    freezeTableName: boolean;
+  };
+  dialectOptions?: {
+    ssl: {
+      require: boolean;
+      rejectUnauthorized: boolean;
+      ca?: string;
+    };
+  };
+}
+
+const sequelizeConfig: SequelizeConfig = {
   dialect: 'postgres',
+  dialectModule: pg, // Явно указываем использование pg
   models: [User, Character, Message, SpiceTransaction],
   logging: false,
   define: {
@@ -17,15 +43,12 @@ const sequelizeConfig: any = {
 };
 
 if (isDigitalOceanDB) {
-  // Для DigitalOcean нужна специфичная настройка
   sequelizeConfig.dialectOptions = {
-    ssl: process.env.DATABASE_CA_CERT
+    ssl: ca
       ? {
           require: true,
-          rejectUnauthorized: false, // Важно для DigitalOcean!
-          ca: Buffer.from(process.env.DATABASE_CA_CERT, 'base64').toString('utf-8'),
-          // Добавляем дополнительные параметры для DigitalOcean
-          checkServerIdentity: () => undefined, // Пропускаем проверку имени сервера
+          rejectUnauthorized: true,
+          ca: ca,
         }
       : {
           require: true,
